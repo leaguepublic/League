031f17ce3bd4a8afc7ed3dbea4c10aeb6812f6da
Lee Jeongseok <makin@hanmail.net>
2018-08-26 23:15:50 +0900

    add icon and layout

---
+++ app/src/main/java/com/welab/league/ui/MakeTeamActivity.java
@@ -0,0 +1,15 @@
+package com.welab.league.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.welab.league.R;
+
+public class MakeTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_team);
+    }
+}
